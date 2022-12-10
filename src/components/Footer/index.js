16f9c19fb9b8632d5e558e0d4ee02f8bc2dae652
Mykon <helvis89@gmail.com>
2022-12-10 10:18:02 -0300

    atualização do footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,16 @@ import './Footer.css';
 import GitHub from '../assets/github.svg';
 import Linkedin from '../assets/linkedin.svg';
 import Instagram from '../assets/instagram.svg';
+import Voltar from '../assets/voltar.svg';
 
 export default function Footer() {
   return (
     <footer>
-      <div className="linkSocial">
+      <div className="copyRigth">
+        <p>Portfólio criado por Hélvis Mykon.</p>
+        <p>Todos os direitos reservados ₢.</p>
+      </div>
+      <div className="redeSocial">
         <a href="https://www.linkedin.com/in/mykon89/">
           <img src={Linkedin} alt="Logo Linkedin" />
         </a>
@@ -16,6 +21,11 @@ export default function Footer() {
         </a>
         <a href="https://www.instagram.com/mykon89/">
           <img src={Instagram} alt="Logo Instagram" />
+        </a>
+      </div>
+      <div className="voltarTopo">
+        <a href="">
+          <img src={Voltar} alt="" />
         </a>
       </div>
     </footer>
